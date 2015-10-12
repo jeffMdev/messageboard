126254ad763ae7842fa35e8972039c256eeb37a1
Jeffrey Merioles <jeffreymerioels@gmail.com>
2015-10-12 17:02:03 +0800

    Adding authentication

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -4,15 +4,25 @@ App::uses('AppController', 'Controller');
 
 class UsersController extends AppController {
 
+	public function beforeFilter() {
+	    parent::beforeFilter();
+	    $this->Auth->allow('register', 'logout');
+	}
+
 	public function index() {
 
 	}
 
 	public function login() {
-		
+		if ($this->request->is('post')) {
+	        if ($this->Auth->login()) {
+	            return $this->redirect($this->Auth->redirectUrl());
+	        }
+	        $this->Flash->error(__('Invalid email or password, please try again.'));
+	    }
 	}
 
 	public function register() {
-		
+		return $this->redirect($this->Auth->logout());
 	}
 }
--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -3,4 +3,12 @@
 App::uses('Model', 'Model');
 
 class UserModel extends Model {
+	// public function beforeSave($options = array()) {
+	//     if (isset($this->data[$this->alias]['password'])) {
+	//         $passwordHasher = new BlowfishPasswordHasher();
+	//         $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
+	//     }
+	//     return true;
+	// }
+
 }
